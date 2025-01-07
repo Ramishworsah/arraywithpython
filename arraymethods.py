e9706b3e3a1696b31919bcd17a20015cbf3c84b5
akhil <ramishworsah574@gmail.com>
2025-01-07 11:19:51 +0530

    simple code for numpy and to deal with array

--- arraymethods.py
+++ arraymethods.py
@@ -1,41 +1,41 @@
 import array
 new_object=array.array('i',[1,2,3,4,5,6,7])
-# print(new_object.itemsize)
-# print(new_object.typecode)
-# new_object.append(8);
-# print(new_object)
+print(new_object.itemsize)
+print(new_object.typecode)
+new_object.append(8);
+print(new_object)
 
-# print(new_object.insert(2,12));  
-# for append the value:
-# print();
-# new_object.append(9)
-# print(new_object)
+print(new_object.insert(2,12));  
+#for append the value:
+print();
+new_object.append(9)
+print(new_object)
 
 
 #todo using extend method
-# y=[1,2,3,4];
-# new_object.extend(y)
-# print(new_object)
+y=[1,2,3,4];
+new_object.extend(y)
+print(new_object)
 
 #?REMOVE ELEMENT FROM ARRAY
-# new_object=array.array('i',[1,2,3,4,5,6,7])
-# new_object.remove(3)
-# print(new_object)
+new_object=array.array('i',[1,2,3,4,5,6,7])
+new_object.remove(3)
+print(new_object)
 #*using reverese method
-# new_object=array.array('i',[1,2,3,4,5,6,7])
-# new_object.reverse()
-# print(new_object)
+new_object=array.array('i',[1,2,3,4,5,6,7])
+new_object.reverse()
+print(new_object)
 
-# NOTE: using pop method
-# new_object=array.array('i',[1,2,3,4,5,6,7])
-# print(new_object.pop())
-# print(new_object)
+#NOTE: using pop method
+new_object=array.array('i',[1,2,3,4,5,6,7])
+print(new_object.pop())
+print(new_object)
 
-# FIXME: using index method
-# new_object.sort() 
+#FIXME: using index method
+new_object.sort() 
 #! sort method can not be used on array 
-# new_object=array.array('i',[1,2,8,4,5,6,7])
-# print(new_object)
+new_object=array.array('i',[1,2,8,4,5,6,7])
+print(new_object)
 
 # todo  using numerical python numpy library
 import numpy as np
@@ -45,4 +45,8 @@ print(new_object)
 print(np.__version__)
 print(pd.__version__)
 
-inrohan
+int_array=np.array([1,2,3,4,5,6,7])
+print(int_array.dtype) #todo it give the data type 
+print(int_array.ndim) #! it give the dimension
+print(int_array.shape) #* it give the shape of the array in form column and row form
+print(int_array.size)  #? it give the size of the array
